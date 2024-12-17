2d3dfa59160eec881f4168fcbe0b3af30731e064
Artem Novikov <novartole@gmail.com>
2024-12-18 00:19:11 +0300

    l1.11: attempt 2

--- l111_temp_intervals/src/lib.rs
+++ l111_temp_intervals/src/lib.rs
@@ -1,80 +1,91 @@
-// No solution: restrictions are too weak.
-// It's easy to create a vec of f64,
-// whose elements DON'T behave as expected.
-#[allow(dead_code)]
-fn explanation() {
-    // 1st: precision
-    {
-        // values from description
-        let mut temps = vec![-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5];
-        let mut ints = split_into_intervals(&temps);
-        // this values don't give any problem
-        assert_eq!(ints[0].0, "[-30, -20)");
-        assert_eq!(ints[0].1.len(), 3);
-        assert_eq!(ints[3].0, "[30, 40)");
-        assert_eq!(ints[3].1.len(), 1);
-
-        // Let's add a few values from [-30, 40) range:
-        temps.extend([
-            // should _defenetly_ go to [-30, -20):
-            //   -20.000000000000001 < -20.0
-            -20.0 - 1e-15,
-            // should _defenetly_ go to [30, 40):
-            //   39.999999999999999 < 40.0
-            40.0 - 1e-15,
-        ]);
-
-        ints = split_into_intervals(&temps);
-        // The following asserts pass,
-        // that means -20.0 - 1e-15 went into [-20, -10):
-        assert_eq!(ints[1].0, "[-20, -10)");
-        assert_eq!(ints[1].1.len(), 1);
-        assert_eq!(ints[0].0, "[-30, -20)");
-        assert_ne!(ints[0].1.len(), 4);
-
-        // and similar for 40 - 1e-15
-        assert_eq!(ints[4].0, "[30, 40)");
-        assert_eq!(ints[4].1.len(), 1);
+/// Split values ('vals') into two vectors.
+///
+/// The first vector contains result ranges as pairs:
+/// - low bound of the range,
+/// - values of the range, which are less than the high range (= low bound + 10.0).
+///
+/// The second vector contains values, which cannot be categorized: ±Infinity, NaN.
+pub fn split(vals: &[f64]) -> (Vec<(f64, Vec<f64>)>, Vec<f64>) {
+    // collect values of each range in map
+    let mut map = std::collections::HashMap::new();
+    // prepare finite values, and defer the rest till the end
+    let (finite_vals, inf_and_nan_vals) =
+        vals.iter().partition::<Vec<f64>, _>(|val| val.is_finite());
+    for val in finite_vals {
+        // use bits representation as key for map
+        let bits = {
+            let offset = if val.is_sign_negative() { -1.0 } else { 0.0 };
+            let low_bound = (val / 10.0 + offset).trunc() * 10.0;
+            low_bound.to_bits()
+        };
+        map.entry(bits)
+            .and_modify(|vals: &mut Vec<_>| vals.push(val))
+            .or_insert(vec![val]);
     }
-
-    // 2nd: too big
-    {
-        let mut big_val = 1e54_f64;
-        for _ in 0..1_000_000 {
-            assert!((big_val % 10.0).abs() > f64::EPSILON);
-            big_val -= 1.0;
-        }
-    }
+    let ranges = map
+        .into_iter()
+        // go back from bits to value
+        .map(|(bits, vals)| (f64::from_bits(bits), vals))
+        .collect();
+    (ranges, inf_and_nan_vals)
 }
 
-/// Go from [-30, -20) to [30, 40) and fill _result_ intervals.
-fn split_into_intervals(temps: &[f64]) -> Vec<(String, Vec<f64>)> {
-    let mut result = vec![];
-
-    for (left, right) in (-30..=30)
-        .step_by(10)
-        .zip((-20..=40).step_by(10))
-        .map(|(left, right)| (left as f64, right as f64))
-    {
-        let temps = Vec::from_iter(
-            temps
-                .iter()
-                .copied()
-                .filter(|&temp| left <= temp && temp < right),
-        );
-
-        if !temps.is_empty() {
-            result.push((format!("[{}, {})", left as i32, right as i32), temps));
-        }
+#[allow(dead_code)]
+fn print_ranges(ranges: &[(f64, Vec<f64>)]) {
+    for (low_bound, vals) in ranges {
+        let high_bound = low_bound + 10.0;
+        println!("[{}, {}): {:?}", low_bound, high_bound, vals);
     }
-
-    result
 }
 
 #[cfg(test)]
-mod l111 {
+mod tests {
+    use super::*;
+
     #[test]
-    fn prove() {
-        super::explanation();
+    fn example() {
+        let values = [-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5];
+        let (mut ranges, skipped_values) = split(&values);
+        ranges.sort_unstable_by(|(a, _), (b, _)| a.total_cmp(b));
+        assert_eq!(
+            ranges,
+            &[
+                (-30.0, vec![-25.4, -27.0, -21.0]),
+                (10.0, vec![13.0, 19.0, 15.5]),
+                (20.0, vec![24.5]),
+                (30.0, vec![32.5])
+            ]
+        );
+        assert_eq!(skipped_values, &[]);
+        print_ranges(&ranges);
+    }
+
+    #[test]
+    fn subnormal() {
+        let values = [
+            f64::MIN_POSITIVE - f64::MIN_POSITIVE / 2.0,
+            -f64::MIN_POSITIVE + f64::MIN_POSITIVE / 3.0,
+        ];
+        let (mut ranges, skipped_values) = split(&values);
+        ranges.sort_unstable_by(|(a, _), (b, _)| a.total_cmp(b));
+        assert_eq!(
+            ranges,
+            &[
+                (-10.0, vec![-f64::MIN_POSITIVE + f64::MIN_POSITIVE / 3.0]),
+                (0.0, vec![f64::MIN_POSITIVE - f64::MIN_POSITIVE / 2.0]),
+            ]
+        );
+        assert_eq!(skipped_values, &[]);
+        print_ranges(&ranges);
+    }
+
+    #[test]
+    fn infinity_and_nan() {
+        let values = [f64::INFINITY, f64::NEG_INFINITY, f64::NAN];
+        let (ranges, skipped_values) = split(&values);
+        assert_eq!(ranges, &[]);
+        for (a, b) in values.into_iter().zip(skipped_values) {
+            assert_eq!(a.total_cmp(&b), std::cmp::Ordering::Equal);
+        }
     }
 }
